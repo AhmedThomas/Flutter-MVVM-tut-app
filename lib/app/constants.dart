015b8806c36d5dc5425b0983684a18d4f950fc3f
Ahmed Thomas <71823119+AhmedThomas@users.noreply.github.com>
2022-09-13 17:46:29 +0200

    api

---
+++ lib/app/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const String baseUrl = "https://ahmedthomas07.mocklab.io/";
+  static const String empty = "";
+  static const int zero = 0;
+}
---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,16 @@
+import 'package:advanced_flutter_mvvm/data/response/responses.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+import '../../app/constants.dart';
+
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login(
+      @Field("email") String email, @Field("password") String password);
+}
